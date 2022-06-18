4d6656cb46da326f161fafe2575f294a600d7252
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-06-18 23:48:34 +0300

    Config of Postgress DB and db connection via SQLAlchemy

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:223656@localhost:5432/store"
+db = SQLAlchemy(app)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
